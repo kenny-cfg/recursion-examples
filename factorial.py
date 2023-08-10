9f1ab14272e3d6d27e94515c21db197185e090e5
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-10 18:51:09 +0100

    Recursive factorial

--- factorial.py
+++ factorial.py
@@ -5,6 +5,13 @@ def factorial(num):
     return product
 
 
+def factorial_recursion(num):
+    return 1 if num <= 0 else factorial_recursion(num - 1) * num # ternary notation
+
+
 if __name__ == '__main__':
     result = factorial(5)  # 5 x 4 x 3 x 2 x 1
     print(result)
+    result_with_recursion = factorial_recursion(5)
+    print(result)
+

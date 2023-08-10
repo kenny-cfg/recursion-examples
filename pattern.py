858853aa6800e912c9ebfc06d2ffa227b727d46a
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-10 18:42:10 +0100

    Tidy up of first example

--- pattern.py
+++ pattern.py
@@ -1,11 +1,9 @@
 def print_pattern(num):
     if num < 1:
         return
-    else:
-        print(num, end=" ")
-        print_pattern(num - 1)
-        print(num, end=" ")
-        return
+    print(num, end=" ")
+    print_pattern(num - 1)
+    print(num, end=" ")
 
 
 # Driver Code

ad56706cefd062037c952abd540007aeb8c7bb1b
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-10 19:38:57 +0100

    Delinting

--- string_combinations.py
+++ string_combinations.py
@@ -2,11 +2,11 @@ def string_combination(char_set, length):
     if length == 0:
         return {''}
     simpler_result = string_combination(char_set, length - 1)
-    result = set()
+    return_value = set()
     for character in char_set:
         for simpler_result_entry in simpler_result:
-            result.add(character + simpler_result_entry)
-    return result
+            return_value.add(character + simpler_result_entry)
+    return return_value
 
 
 if __name__ == '__main__':

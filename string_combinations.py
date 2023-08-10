95d15e1005a9158a13afac0e2863c29cb5082f49
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-10 19:38:37 +0100

    Better exit condition

--- string_combinations.py
+++ string_combinations.py
@@ -1,6 +1,6 @@
 def string_combination(char_set, length):
-    if length == 1:
-        return char_set
+    if length == 0:
+        return {''}
     simpler_result = string_combination(char_set, length - 1)
     result = set()
     for character in char_set:
@@ -8,33 +8,7 @@ def string_combination(char_set, length):
             result.add(character + simpler_result_entry)
     return result
 
-"""
-Exit condition:
-* If length = 0, return [ '' ]
-{ 'a', 'b' }, length = 0 -> [ '' ]
 
-Recursion:
-* Work out the combinations with length as length - 1
-* Prepend in turn the elements of char_set
-* Add all those resulting strings to a new set
-
-Concrete example:
-char_set = {'a', 'b'}, length = 3
-
-length = 2 gives:
-  aa
-  ab
-  ba
-  bb
-
-Prepend with each of char_set in turn:
-  a + aa
-  a + ab
-  a + ba
-  a + bb
-  
-  b + aa
-  b + ab
-  b + ba
-  b + bb
-"""
+if __name__ == '__main__':
+    result = string_combination({'a', 'b', 'c'}, 10)
+    print(result)
